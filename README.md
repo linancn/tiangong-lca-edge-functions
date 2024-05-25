2111d7bb6044575ecb72c77bb0c365a0575cc4a0
nanli <linanenv@gmail.com>
2024-05-25 18:21:31 +0800

    add embedding

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20
 npm i supabase --save-dev
+npx supabase start
+
 ```
 
 ## Supabase Config
@@ -19,14 +21,14 @@ npx supabase functions deploy hello --project-ref qgzvkongdjqiiamzbbts
 ```
 ## local
 
-USE VSCode Debug -> Deno
+USE VSCode Debug to start Server -> Launch Supabase Function
 
 
 ## Remote
 
 ```bash
-curl --request POST 'https://qgzvkongdjqiiamzbbts.supabase.co/functions/v1/embedding' \
-  --header 'Authorization: Bearer XXX' \
+curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/embedding' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
   --header 'Content-Type: application/json' \
-  --data '{ "name":"Functions" }'
+  --data '{"query":["Hello", "World"]}'
 ```
--- supabase/functions/embedding/index.ts
+++ supabase/functions/embedding/index.ts
@@ -1,32 +1,39 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
-/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
-
-console.log("Hello from Functions!")
+import OpenAI from "https://deno.land/x/openai@v4.47.1/mod.ts";
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  const { query } = await req.json();
+  const apiKey = Deno.env.get("OPENAI_API_KEY");
+
+  const openai = new OpenAI({
+    apiKey: apiKey,
+  });
+
+  async function getEmbedding(query: Array<string>) {
+    try {
+      const response = await openai.embeddings.create({
+        input: query,
+        model: "text-embedding-3-small",
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error creating embedding:", error);
+      return { error: "Failed to create embedding" };
+    }
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  const embeddingResult = await getEmbedding(query);
 
+  return new Response(JSON.stringify(embeddingResult), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
 /* To invoke locally:
 
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:8000/functions/v1/embedding' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
+curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/embedding' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+  --header 'Content-Type: application/json' \
+  --data '{"query":["Hello", "World"]}'
 */
