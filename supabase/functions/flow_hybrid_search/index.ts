a85b479693c5c36df461a4798af23f1eb38dec50
hyy1987 <779003812@qq.com>
2025-06-13 00:58:36 +0800

    feat: Update VSCode settings for TypeScript and JavaScript formatting, and upgrade Supabase and TypeScript dependencies

--- supabase/functions/flow_hybrid_search/index.ts
+++ supabase/functions/flow_hybrid_search/index.ts
@@ -1,5 +1,5 @@
 // Setup type definitions for built-in Supabase Runtime APIs
-import '@supabase/functions-js/edge-runtime.d.ts';
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
 
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { ChatOpenAI } from '@langchain/openai';
@@ -95,18 +95,12 @@ Task: Transform description of flows into three specific queries: SemanticQueryE
 
   const res = await chain.invoke({ input: query });
 
-  // console.log({ res });
-
   const combinedFulltextQueries = [...res.fulltext_query_zh, ...res.fulltext_query_en].map(
     (query) => `(${query})`,
   );
   const queryFulltextString = combinedFulltextQueries.join(' OR ');
 
-  // console.log(queryFulltextString);
-
   const semanticQueryEn = res.semantic_query_en;
-
-  // console.log(semanticQueryEn);
 
   const session = new Supabase.ai.Session('gte-small');
   const vectors = (await session.run(semanticQueryEn, {
@@ -115,16 +109,11 @@ Task: Transform description of flows into three specific queries: SemanticQueryE
   })) as number[];
   const vectorStr = `[${vectors.toString()}]`;
 
-  // console.log("vectorStr", vectorStr);
-
-  const filter_condition = filter;
-
   const { data, error } = await supabase.rpc('hybrid_search_flows', {
     query_text: queryFulltextString,
     query_embedding: vectorStr,
     ...(filter !== undefined ? { filter_condition: filter } : {}),
   });
-  // console.log({ data, error });
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
@@ -132,9 +121,27 @@ Task: Transform description of flows into three specific queries: SemanticQueryE
       status: 500,
     });
   }
-
-  return new Response(JSON.stringify(data), {
-    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+  if (data) {
+    if (data.length > 0) {
+      return new Response(
+        JSON.stringify({
+          data,
+        }),
+        {
+          headers: {
+            ...corsHeaders,
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        },
+      );
+    }
+  }
+  return new Response('[]', {
+    headers: {
+      ...corsHeaders,
+      'Content-Type': 'application/json',
+    },
     status: 200,
   });
 });
